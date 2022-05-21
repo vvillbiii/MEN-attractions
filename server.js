fec6157c21165592e955cf2484c96310e09b32ec
vvillbii <will.bowles90@gmail.com>
2022-05-20 21:52:15 -0400

    created user controller, and refactored server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dbConnection = require("./config/db.connections");
 const methodOverride = require("method-override");
 const PORT = 4000;
 const controllers = require("./controllers/attractions");
+const user = require("./controllers/user");
 
 app.set("view engine", "ejs");
 
@@ -12,17 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/attractions", controllers);
+app.use("/", user);
 
 app.get("/", (req, res) => {
   res.render("homepage");
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
 });
 
 app.get("/*", (req, res) => {
